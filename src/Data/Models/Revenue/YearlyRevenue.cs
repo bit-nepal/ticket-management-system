70ee039b6b88f336337bf24528ee47e793d30d76
manzilstha07 <manzilstha12@gmail.com>
2024-12-05 22:58:33 +0545

    Print preview/ database rollback

--- src/Data/Models/Revenue/YearlyRevenue.cs
+++ src/Data/Models/Revenue/YearlyRevenue.cs
@@ -5,8 +5,26 @@ public class YearlyRevenue
   public int Year { get; set; } // Nepali fiscal year
 
   public List<MonthlyRevenue> MonthlyRevenues { get; set; } = new();
+    public int? GetTotalPeople(RevenueType revenueType)
+    {
+        int result = MonthlyRevenues
+            .SelectMany(mr => mr.DailyRevenues)
+            .SelectMany(dr => dr.RevenueCells)
+            .Where(rc => rc.Type == revenueType)
+            .Sum(rc => rc.NoOfPeople);
+        return result;
+    }
 
-  public int TotalPeople => MonthlyRevenues.Sum(mr => mr.TotalPeople);
+    public int? GetTotalRevenue(RevenueType revenueType)
+    {
+        int result = MonthlyRevenues
+            .SelectMany(mr => mr.DailyRevenues)
+            .SelectMany(dr => dr.RevenueCells)
+            .Where(rc => rc.Type == revenueType)
+            .Sum(rc => rc.TotalAmount);
+        return result;
+    }
+    public int TotalPeople => MonthlyRevenues.Sum(mr => mr.TotalPeople);
   public int TotalRevenue => MonthlyRevenues.Sum(mr => mr.TotalRevenue);
 }
 
---
+++ src/Migrations/20241205074900_ticket3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tms.Migrations
+{
+    /// <inheritdoc />
+    public partial class ticket3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TicketNoEnd",
+                table: "DailyRevenues",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicketNoStart",
+                table: "DailyRevenues",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TicketNoEnd",
+                table: "DailyRevenues");
+
+            migrationBuilder.DropColumn(
+                name: "TicketNoStart",
+                table: "DailyRevenues");
+        }
+    }
+}
